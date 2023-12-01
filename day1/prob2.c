cf5d299cca43bd299079ced43926ccb30d1efd84
99xtal <joe.rybarczyk@me.com>
2023-12-01 13:19:09 -0500

    refactor(d1p2): combine loops

--- day1/prob2.c
+++ day1/prob2.c
@@ -27,81 +27,60 @@ char* strstr_last(char *s, char *sub) {
     return last;
 }
 
-int first_digit_in_string(char* line) {
-    int first_digit = -1;
-    int first_digit_index = -1;
-
-    // Find index of first occurance of one of the digit strings
-    int arr_len = sizeof(digit_strings) / sizeof(char*);
-    for (int i = 1; i < arr_len; i++) {
-        char* substring = strstr(line, digit_strings[i]);
-
-        if (substring != NULL) { 
-            int index = substring - line;
-            if (index < first_digit_index || first_digit_index == -1) {
-                first_digit_index = index;
-                first_digit = i;
-            }
-        }
-    }
-    
-    // Find index of first occurance of one of the digit characters
-    for (int i = 0; i <= 9; i++) {
-        char num = '0' + i;
-        char* first_char = strchr(line, num);
-        if (first_char != NULL) {
-            int index = first_char - line;
-            if (index < first_digit_index || first_digit_index == -1) {
-                first_digit_index = index;
-                first_digit = i; 
-            }
-        }
-    }
-
-    return first_digit;
-}
-
-int last_digit_in_string(char *line) {
-    int last_digit = -1;
-    int last_digit_index = -1;
-
-    // Find index of last occurance of one of the digit strings
-    int arr_len = sizeof(digit_strings) / sizeof(char*);
-    for (int i = 1; i < arr_len; i++) {
-        char* substring = strstr_last(line, digit_strings[i]);
-
-        if (substring != NULL) { 
-            int index = substring - line;
-            if (index > last_digit_index || last_digit_index == -1) {
-                last_digit_index = index;
-                last_digit = i; 
-            }
-        }
-    }
-    
-    // Find index of last occurance of one of the digit characters
-    for (int i = 0; i <= 9; i++) {
-        char num = '0' + i;
-        char* num_char = strrchr(line, num);
-        if (num_char != NULL) {
-            int index = num_char - line;
-            if (index > last_digit_index || last_digit_index == -1) {
-                last_digit_index = index;
-                last_digit = i; 
-            }
-        }
-    }
-
-    return last_digit;
-}
-
 int main() {
     int sum = 0;
     char line[MAXLINE];
     int first_digit = 0;
+    int first_digit_index = -1;
     int last_digit = 0;
+    int last_digit_index = -1;
 
     while(fgets(line, sizeof(line) / sizeof(char), stdin) != NULL) {
+        // Check numeric substrings
+        for (int i = 1; i < sizeof(digit_strings) / sizeof(char*); i++) {
+
+            // Get first and last occurence of numeric strings in line
+            char* first_substr = strstr(line, digit_strings[i]);
+            if (first_substr != NULL) { 
+                int index = first_substr - line;
+                if (index < first_digit_index || first_digit_index == -1) {
+                    first_digit_index = index;
+                    first_digit = i;
+                }
+            }
+
+            char* last_substr = strstr_last(line, digit_strings[i]);
+            if (last_substr != NULL) { 
+                int index = last_substr - line;
+                if (index > last_digit_index || last_digit_index == -1) {
+                    last_digit_index = index;
+                    last_digit = i; 
+                }
+            }
+        }
+
+        // Check numeric characters
+        for (int i = 0; i <= 9; i++) {
+            char num_char = '0' + i;
+
+           // Get first and last occurence of digit character in line
+            char* first_num_substr = strchr(line, num_char);
+            if (first_num_substr != NULL) {
+                int index = first_num_substr - line;
+                if (index < first_digit_index || first_digit_index == -1) {
+                    first_digit_index = index;
+                    first_digit = i;
+                }
+            }
+            char* last_num_substr = strrchr(line, num_char);
+            if (last_num_substr != NULL) {
+                int index = last_num_substr - line;
+                if (index > last_digit_index || last_digit_index == -1) {
+                    last_digit_index = index;
+                    last_digit = i; 
+                }
+            }
+        }
         first_digit = first_digit_in_string(line);
         last_digit = last_digit_in_string(line);
         sum += (first_digit * 10) + last_digit;
