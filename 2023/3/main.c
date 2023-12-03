793faec754d66af2a5e4fd56f000171098bfa649
99xtal <joe.rybarczyk@me.com>
2023-12-03 16:05:03 -0500

    feat(d3): day 3 prob 2 solution

--- 2023/3/main.c
+++ 2023/3/main.c
@@ -25,6 +25,21 @@ int max(int a, int b) {
     }
 }
 
+typedef struct position {
+    int x;
+    int y;
+} position;
+
+typedef struct position_node {
+    position *pos;
+    struct position_node *next;
+} position_node;
+
+typedef struct num_node {
+    int num;
+    struct num_node *next;
+} num_node;
+
 bool is_near_symbol(int num_x, int num_y, int num_len, char** schem) {
     int y_start = max(num_y - 1, 0);
     int y_end = min(num_y + 1, SCHEM_H - 1);
@@ -39,6 +54,65 @@ bool is_near_symbol(int num_x, int num_y, int num_len, char** schem) {
         }
     }
     return false;
+}
+
+num_node* get_adjacent_nums(position* source, char** schem) {
+    int x_min = max(source->x - 1, 0);
+    int x_max = min(source->x + 1, SCHEM_W - 1);
+    int y_min = max(source->y - 1, 0);
+    int y_max = min(source->y + 1, SCHEM_H - 1);
+    char cursor;
+    position_node* num_positions = NULL;
+
+    int x, y;
+    for (y = y_min; y <= y_max; y++) {
+        for (x = x_min; x <= x_max; x++) {
+            cursor = schem[y][x];
+            if (isdigit(cursor)) {
+                int i = x;
+                while(isdigit(schem[y][max(i-1,0)]) && i > 0) {
+                    i--;
+                }
+
+                position* num_i = (position*)malloc(sizeof(position));
+                num_i->x = i;
+                num_i->y = y;
+
+                int dupes_n = 0;
+                for (position_node* p = num_positions; p != NULL; p = p->next) {
+                    if (p->pos->x == num_i->x && p->pos->y == num_i->y) {
+                        dupes_n++;
+                    }
+                }
+
+                if (dupes_n == 0) {
+                    position_node* new_pos = (position_node*)malloc(sizeof(position_node));
+                    new_pos->pos = num_i;
+                    new_pos->next = num_positions;
+                    num_positions = new_pos;
+                }
+            }
+        }
+    }
+
+    num_node* nums = NULL;
+    for (position_node* p = num_positions; p != NULL; p = p->next) {
+        int num = 0;
+        int x = p->pos->x;
+        int y = p->pos->y;
+        char cursor = schem[y][x];
+        while(isdigit(cursor)) {
+            num = (num * 10) + (cursor - '0');
+            x++;
+            cursor = schem[y][x];
+        }
+        num_node* new_node = (num_node*)malloc(sizeof(num_node));
+        new_node->num = num;
+        new_node->next = nums;
+        nums = new_node;
+    }
+
+    return nums;
 }
 
 int main() {
@@ -58,6 +132,7 @@ int main() {
         i++;
     }
 
+    // Part 1 solution
     char cursor;
     int num_index = -1;
     int num_length;
@@ -91,6 +166,34 @@ int main() {
         }
     }
 
-    printf("%d\n", parts_sum);
+    // Part 2 solution
+    int gear_ratio_sum = 0;
+    for (int i = 0; i < SCHEM_H; i++) {
+        for (int j = 0; j < SCHEM_W; j++) {
+            cursor = schem[i][j];
+            num_node* nums;
+
+            if (cursor == '*') {
+                position p;
+                p.x = j;
+                p.y = i;
+                nums = get_adjacent_nums(&p, schem);
+
+                int gear_ratio = 1;
+                int num_part_nums = 0;
+                for (num_node* p = nums; p != NULL; p = p->next) {
+                    gear_ratio *= p->num;
+                    num_part_nums++;
+                }
+                
+                if (num_part_nums == 2) {
+                    gear_ratio_sum += gear_ratio;
+                }
+            }
+        }
+    }
+
+    printf("Part number sum: %d\n", parts_sum);
+    printf("Gear ratio sum: %d\n", gear_ratio_sum);
     return 0;
 }
